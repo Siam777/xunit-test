dcaf78ef345627be83b4036f65a34048cb64d688
siamriaz <siam.riaz@selise.ch>
2024-04-30 14:55:23 +0600

    initial commit

---
+++ XUnitDemo.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitDemo.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeController_Index_ValidResult()
+        {
+
+        }
+    }
+}
---
+++ XUnitDemo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XUnitDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "I am in home";
+        }
+    }
+}
